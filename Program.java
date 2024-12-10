2f3444017631034b1156394e5fb8216b8fd9e895
Ruan Oliveira da Silva <69682432+Dracofer@users.noreply.github.com>
2024-12-10 16:19:33 -0300

    Program.java

--- Program.java
+++ Program.java
@@ -13,6 +13,7 @@ public class Program {
 		System.out.print("Quantas pessoas serao digitadas?");
 		int n = sc.nextInt();
 		Product[] vect = new Product[n];
+		double idade = 0;
 		
 		for (int i = 0; i < n; i++) {
 			sc.nextLine();
@@ -20,21 +21,29 @@ public class Program {
 			System.out.print("Nome:");
 			String name = sc.nextLine();
 			System.out.print("Idade:");
-			double idade = sc.nextDouble();
+			idade = sc.nextDouble();
 			System.out.print("Altura:");
 			double altura = sc.nextDouble();
 			vect[i] = new Product(name, idade, altura);
 		}
 		
 		double sum = 0.0;
+		double pMenores = 0; 
+		double nMenores = 0;
 		
 		for (int i = 0; i < n; i++) {
 			sum += vect[i].getAltura();
+			pMenores = ((double)nMenores / n) * 100.0;
 		}
-		
+		for (int i=0; i<n; i++) {
+	        if (vect[i].getIdade() < 16) {
+	            nMenores++;
+	        }
+	        pMenores = ((double)nMenores / n) * 100.0;
+		}
 		double avg = sum / n;
 		System.out.printf("Altura média: %.2f%n", avg);
-		System.out.println("Pessoas com menos de 16 anos: 40.0%");
+		System.out.printf("Pessoas com menos de 16 anos: %.1f%%\n", pMenores);
 		for (int i = 0; i < n; i++) {
 			if(vect[i].getIdade() < 16) {
 				System.out.println(vect[i].getName());
